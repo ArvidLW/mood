980334a8275c6b4e04bd7f2e5af4fbf1b4090dd0
lw <lw_coming@163.com>
2016-07-13 23:00:42 +0800

    改改状态不好

--- app/src/main/java/com/study/zlwm/mood/FragmentMoodPlan.java
+++ app/src/main/java/com/study/zlwm/mood/FragmentMoodPlan.java
@@ -1,5 +1,6 @@
 package com.study.zlwm.mood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,12 +8,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 31351 on 2016/7/5.
@@ -21,10 +26,13 @@ public class FragmentMoodPlan extends Fragment {
     @Nullable
     private View myview;
     private ListView listView;
+    private String[] weeks =new String[]{"星期一","星期二","星期三","星期四","星期五","星期六","星期天"};
+    private String[] tags = new String[]{"淡然","无所谓","坚定","从容","奋斗","青城山","泸沽湖"};
+    private String[] simCons = new String[]{"看开看开，风吹裤裆","心有千千节，小事","学习学习……","我的沧海呢","100……","好地方哦","女儿国"};
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-       // myview=inflater.inflate(R.layout.fragment_mood_plan,container,false);
+        // myview=inflater.inflate(R.layout.fragment_mood_plan,container,false);
         //View rootView = inflater.inflate(R.layout.fragment_mood_plan, null);
         //Log.d("sdfsdf","进入Fragment页面");
         /*listView= (ListView) myview.findViewById(android.R.id.list);
@@ -43,19 +51,50 @@ public class FragmentMoodPlan extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        listView= (ListView) view.findViewById(android.R.id.list);
-        /*if(listView==null)
-        {
-            Log.d("sdfsdf","sdfsdflwwwwwwlwww");
-        }
-        else {
-            Log.d("sdfsdf","nnnnsdfsdflwwwwwwlwww");
+        listView= (ListView) view.findViewById(R.id.mood_plan_list);
 
-        }*/
-        String[] array = {"a","b","c","d","e","f","g"};
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, array);
+        List<Map<String,Object>> listItems = new ArrayList<Map<String,Object>>();
+        for(int i=0;i<weeks.length;++i)
+        {
+            Map<String,Object> listItem= new HashMap<>();
+            listItem.put("week",weeks[i]);
+            listItem.put("tag",tags[i]);
+            listItem.put("simCon",simCons[i]);
+            listItems.add(listItem);
+        }
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(), listItems, R.layout.mood_plan_item,
+                new String[]{"week","tag","simCon"},
+                new int[]{ R.id.week, R.id.moodTag, R.id.moodSimCon} );
+        listView.setAdapter(simpleAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(position);
+                Intent intent=new Intent();
+                intent.setClass(getContext(),EditMoodPlan.class);
+                startActivity(intent);
+
+                //view.setVisibility(View.INVISIBLE);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //View simContent=view.findViewById(R.id.detailInfo);
+                //simContent.setVisibility(View.INVISIBLE);
+                System.out.println(position);
+                view.setVisibility(View.INVISIBLE);
+                Log.d("view",view.toString());
+                //simContent.setMinimumHeight(0);
+                return false;
+            }
+        });
+        //String[] array = {"a","b","c","d","e","f","g"};
+        //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, array);
         //Log.d("eeee",listView.toString());
-        listView.setAdapter(adapter);
+        //listView.setAdapter(adapter);
 
     }
     /*
